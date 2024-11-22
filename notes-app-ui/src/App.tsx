8f8712bfea1f8c1921c879eec2afc926fc364778
ryanloehr1 <ryanloehr1@gmail.com>
2024-11-22 17:38:37 -0500

    First commit of local to github. Initial changes only to App.css and App.tsx files. Rest of files self-created by VS solution

---
+++ notes-app-ui/src/App.tsx
@@ -0,0 +1,33 @@
+import "./App.css"
+
+const App = () => {
+    return (
+        <div className="app-container">
+            <form className="note-form">
+                <input
+                    placeholder="Title"
+                    required
+                ></input>
+                <textarea
+                    placeholder="Content"
+                    rows={10}
+                    required
+                ></textarea>
+                <button type="submit" >
+                    Add Note
+                </button>
+            </form>
+            <div className="notes-grid">
+                <div className="note-item">
+                    <div className="notes-header">
+                        <button>x</button>
+                    </div>
+                    <h2>Note Title</h2>
+                    <p>Note Content</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default App;
