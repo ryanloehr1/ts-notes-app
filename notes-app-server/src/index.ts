b579526aa8f8fcb123a9d2c092ab72ebfe9cb847
ryanloehr1 <ryanloehr1@gmail.com>
2024-11-26 12:18:42 -0500

    Configured prisma to successfully connect to a supabase PostgreSQL DB. Prisma migrations setup auto-generated

--- notes-app-server/src/index.ts
+++ notes-app-server/src/index.ts
@@ -2,12 +2,15 @@ import express from "express"
 import cors from "cors"
 
 const app = express();
+const prisma = new PrismaClient();
 
 app.use(express.json());
 app.use(cors());
 
 app.get("/api/notes", async (req, res) => {
-    res.json({ message: "success!" });
+    const notes = await prisma.note.findMany();
+
+    res.json(notes);
 });
 
 app.listen(5000, () => {
